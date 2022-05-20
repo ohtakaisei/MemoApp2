e10a4c8ac6c79b3757779230d7252619f8dcc991
ohtakaisei <blogsenmonyou@gmail.com>
2022-05-20 14:34:01 +0900

    Install @expo/vector-icons

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, ScrollView, Text, StyleSheet } from "react-native";
+import {
+     View, ScrollView, Text, StyleSheet, 
+} from "react-native";
 
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
@@ -17,7 +19,7 @@ export default function MemoDetailScreen() {
                     トイレットペッパーが一個にっこ三個必要だと思わないのかい君は
                 </Text>
             </ScrollView>
-            <CircleButton style={{top: 160, bottom: "auto"}}>+</CircleButton>
+            <CircleButton style={{top: 160, bottom: "auto"}} name="edit-2" />
         </View>
     );
 }
